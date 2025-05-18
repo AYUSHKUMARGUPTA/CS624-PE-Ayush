4bea44e0156291766dbc2245a0571e5d4331425c
Ayush Kumar Gupta <ayushkg97@gmail.com>
2025-05-17 23:19:28 -0700

    Submission for Part 2 Module06 - Ayush Kumar Gupta

--- PE04-ProfileCard/Part1/Styling/app/(tabs)/App.js
+++ PE04-ProfileCard/Part1/Styling/app/(tabs)/App.js
@@ -1,74 +1,170 @@
-import { Component } from "react";
-import { View, StyleSheet, Image, Text } from "react-native";
+import React, { Component } from "react";
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Animated,
+  Easing,
+  FlatList,
+  Dimensions,
+} from "react-native";
+
+const profileCardColor = "dodgerblue";
+const userImage = require("../../assets/images/user.png");
+const screenWidth = Dimensions.get("window").width;
+const CARD_WIDTH = screenWidth / 2 - 500;
+const CARD_HEIGHT = 220;
+
+class ProfileCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animation: new Animated.Value(0.3),
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.expanded !== this.props.expanded) {
+      Animated.timing(this.state.animation, {
+        toValue: this.props.expanded ? 1 : 0.3,
+        duration: 300,
+        useNativeDriver: false,
+        easing: Easing.inOut(Easing.ease),
+      }).start();
+    }
+  }
+
+  render() {
+    const { onPress } = this.props;
+    const { animation } = this.state;
+
+    const scale = animation.interpolate({
+      inputRange: [0.3, 1],
+      outputRange: [0.8, 1],
+    });
+
+    return (
+      <TouchableWithoutFeedback onPress={onPress}>
+        <Animated.View style={[styles.cardContainer, { transform: [{ scale }] }]}>
+          <View style={styles.cardImageContainer}>
+            <Image style={styles.cardImage} source={userImage} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.nameText}>John Doe</Text>
+            <Text style={styles.roleText}>React Native Developer</Text>
+            <Text style={styles.bioText}>
+              John is a really great JavaScript developer. He loves using JS to
+              build React Native applications for iOS and Android.
+            </Text>
+          </View>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
 
 export default class App extends Component {
+  state = {
+    selectedIndex: null,
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.cardImageContainer}>
-                        <Image style={styles.cardImage} source={require('../../assets/images/user.png')} />
-                    </View>
-                    <View style={styles.cardContentContainer}>
-                        <Text style={styles.name}>John Doe</Text>
-                        <Text style={styles.jobTitle}>React Native Developer</Text>
-                        <Text style={styles.jobDescription}>John is a really great Javascript Developer. He love using JS to build React Native applications for iOS and Android.</Text>
-                    </View>
-                </View>
-            </View>
-        )
-    }
+  handleCardPress = (index) => {
+    this.setState({
+      selectedIndex: index === this.state.selectedIndex ? null : index,
+    });
+  };
+
+  renderItem = ({ item, index }) => (
+    <ProfileCard
+      key={index}
+      expanded={this.state.selectedIndex === index}
+      onPress={() => this.handleCardPress(index)}
+    />
+  );
+
+  render() {
+    return (
+      <FlatList
+        contentContainerStyle={styles.container}
+        data={Array.from({ length: 6 })}
+        renderItem={this.renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+      />
+    );
+  }
 }
-const profileCardColor = 'dodgerblue';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-    },
-    cardContainer: {
-        borderColor: 'black',
-        borderWidth: 3,
-        borderStyle: 'solid',
-        borderRadius: 20,
-        backgroundColor: profileCardColor,
-        width: 300,
-        height: 400,
-        alignItems: 'center',
-    },
-    cardContentContainer: {
-        marginTop: 5,
-        alignItems: 'center',
-    },
-    cardImageContainer: {
-        backgroundColor: 'white',
-        borderWidth: 3,
-        borderColor: 'black',
-        width: 120,
-        height: 120,
-        borderRadius: 60,
-        alignItems: 'center',
-        marginTop: 30,
-        paddingTop: 15
-    },
-    cardImage: {
-        width: 80,
-        height: 80,
-    },
-    name: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    jobTitle: {
-        color: 'black',
-        textDecorationLine: 'underline',
-        fontWeight: 'bold',
-    },
-    jobDescription: {
-
-    }
-})
+  container: {
+    padding: 8,
+    justifyContent: "center",
+  },
+  cardContainer: {
+    backgroundColor: profileCardColor,
+    width: CARD_WIDTH,
+    height: CARD_HEIGHT,
+    margin: 8,
+    borderRadius: 16,
+    borderWidth: 2,
+    borderColor: "black",
+    alignItems: "center",
+    overflow: "hidden",
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 0.3,
+        shadowRadius: 10,
+      },
+      android: {
+        elevation: 10,
+      },
+    }),
+  },
+  cardImageContainer: {
+    alignItems: "center",
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "black",
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginTop: 12,
+    justifyContent: "center",
+  },
+  cardImage: {
+    width: 40,
+    height: 40,
+  },
+  textContainer: {
+    padding: 10,
+    alignItems: "center",
+  },
+  nameText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 8,
+    textShadowColor: "black",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 2,
+  },
+  roleText: {
+    color: "black",
+    fontSize: 12,
+    fontWeight: "bold",
+    marginVertical: 2,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    paddingBottom: 2,
+  },
+  bioText: {
+    color: "black",
+    fontSize: 10,
+    textAlign: "center",
+  },
+});
--- PE04-ProfileCard/Part1/Styling/app/(tabs)/styles.js
+++ PE04-ProfileCard/Part1/Styling/app/(tabs)/styles.js
@@ -1,53 +1,22 @@
-import { StyleSheet } from 'react-native';
-
-// const styles = StyleSheet.create({
-//   container: { 
-//     flex: 1,
-//     paddingTop: 150,
-//     backgroundColor: '#ededed'
-//   },
-//   countContainer: {
-//     alignItems: 'center',
-//     padding: 10,
-//   },
-//   countText: {
-//     fontSize: 18, 
-//     color: 'red'
-//  }
-// })
-
-// const buttons = StyleSheet.create({
-//   primary: {
-//     height: 70,
-//     backgroundColor: 'red',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     marginLeft: 20,
-//     marginRight: 20
-//   },
-//   buttonText: {
-//     color: 'white',
-//   }
-// })
+import {StyleSheet} from "react-native";
 
 export const Colors = {
-    dark: 'black',
-    light: 'white',
+    dark : 'black',
+    light : 'white'
 }
 
 const baseContainerStyles = {
-    justifyContent: 'center',
-    alignItems: 'center',
     flex: 1,
-}
+    justifyContent: "center",
+    alignItems: "center"
+};
 
 const baseBoxStyles = {
-    justifyContent: 'center',
-    alignItems: 'center',
     width: 150,
     height: 150,
-    borderWidth: 2
-}
+    justifyContent: "center",
+    alignItems: "center"
+};
 
 const lightStyleSheet = StyleSheet.create({
     container: {
@@ -56,9 +25,9 @@ const lightStyleSheet = StyleSheet.create({
     },
     box: {
         ...baseBoxStyles,
-        borderColor: Colors.dark
+        backgroundColor: Colors.light
     }
-})
+});
 
 const darkStyleSheet = StyleSheet.create({
     container: {
@@ -67,11 +36,12 @@ const darkStyleSheet = StyleSheet.create({
     },
     box: {
         ...baseBoxStyles,
-        borderColor: Colors.light
+        backgroundColor: Colors.dark
     }
-})
-export default function getStyles(useDarkTheme){
-    return useDarkTheme ? darkStyleSheet : lightStyleSheet
+});
+
+export default function getStyleSheet(useDarkTheme){
+    return useDarkTheme ? darkStyleSheet : lightStyleSheet;
 }
 
-// export { styles, buttons };
+
